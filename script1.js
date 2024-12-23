c0ebd5c76e4422b0d42768ebc20c07fa4e85dcd7
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-23 18:03:26 +0530

    asdrg

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 let data = [
     {
         "id": 1,
-        "list":"todo first",
+        "list":"Todo Listy",
         "status": false
     }
 ]
@@ -57,23 +57,24 @@ function dele(id){
     showData(deleData)
 }
 
-// function statis(id){
-//     let statusData = data.filter((ele) => {
-//         if(ele.status = !ele.status){
-            
-//         }
-//     })
-//     console.log(statusData)
-
-//     showData(statusData)
-// }
-
+function check(id){
+    let statusData = data.map((ele) => {
+        if(ele.id === id){
+            ele.status = !ele.status;
+        }
+         return ele;
+    })
+    
+     showData(statusData);
+    
+    
+    }
 function update(id){
     let updatedData = data.filter((ele) =>{
         if( ele.id == id){
             
-    document.getElementById("todo_list").value = ele.list
-    document.getElementById("id").value = ele.id
+        document.getElementById("todo_list").value = ele.list
+        document.getElementById("id").value = ele.id
         }
     } )
 
@@ -86,38 +87,23 @@ function update(id){
 
 function showData(data){
     tbody.innerHTML=""
-  
-data.map((ele) => {
 
-    tbody.innerHTML += `
-          <tr class="d-flex justify-content-evenly align-items-center  fs-4">
-                        <td class="">
-<div class="box ">
+    data.map((ele) => {
 
-<div class="checkbox-wrapper">
-  <input checked="" type="checkbox" onclick="statis(${ele.id})">
-  <svg viewBox="0 0 35.6 35.6">
-    <circle class="background" cx="17.8" cy="17.8" r="17.8"></circle>
-    <circle class="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
-    <polyline class="check" points="11.78 18.12 15.55 22.23 25.17 12.87"></polyline>
-  </svg>
-</div>
-
-
-
-
-
-</div>
-
-
-
-</td>
-                        <td>${ele.list}</td>
-                        <td><button onclick="update(${ele.id})" class="btn btn-yellow fs-2" ><i class="ri-ball-pen-line"></i></button></td>
-                        <td><button onclick="dele(${ele.id})" class="btn btn-red fs-2" ><i class="ri-close-large-line"></i></i></button></td>
-                    </tr>
-    `
-})
+        tbody.innerHTML += `
+                       <tr class=" fs-4     ${ele.status ? 'table-success' : 'table-danger' }" id="box">
+                            <td class="" >
+   <input id="stat" onchange="check(${ele.id})" value="true" type="checkbox" ${ele.status ? "checked" : ""} class="status1" />
+    
+    
+    
+    </td>
+                            <td>${ele.list}</td>
+                            <td><button onclick="update(${ele.id})" class="btn btn-yellow fs-2" ><i class="ri-ball-pen-line"></i></button></td>
+                            <td><button onclick="dele(${ele.id})" class="btn btn-red fs-2" ><i class="ri-close-large-line"></i></i></button></td>
+                        </tr>
+        `
+    })
 }
 
 
@@ -126,26 +112,11 @@ data.map((ele) => {
 data.map((ele) => {
 
     tbody.innerHTML += `
-          <tr class="d-flex justify-content-evenly align-items-center  fs-4">
-                        <td class="">
-<div class="box ">
 
-<div class="checkbox-wrapper">
-  <input checked="" type="checkbox" onclick="statis(${ele.id})">
-  <svg viewBox="0 0 35.6 35.6">
-    <circle class="background" cx="17.8" cy="17.8" r="17.8"></circle>
-    <circle class="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
-    <polyline class="check" points="11.78 18.12 15.55 22.23 25.17 12.87"></polyline>
-  </svg>
-</div>
-
-
-
-
-
-</div>
-
-
+          <tr class="  fs-4     ${ele.status ? 'table-success ' : 'table-danger' }" id="box">
+                     
+<td class="" >
+<input id="stat" onchange="check(${ele.id})" value="true" type="checkbox" ${ele.status ? "checked" : ""} class="status1 " />
 
 </td>
                         <td>${ele.list}</td>
@@ -154,3 +125,17 @@ data.map((ele) => {
                     </tr>
     `
 })
+
+
+setInterval( function(){
+  
+    
+
+let time = new Date()
+document.getElementById("time").innerHTML = `
+ ${time.getHours()}
+${time.getMinutes()}
+${time.getSeconds()}
+`
+},1000
+)
