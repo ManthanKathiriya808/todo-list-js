9a254e2336feb05ecb5b2849d781f97b9e5779cb
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-24 16:48:26 +0530

    sfdhg

--- script.js
+++ script.js
@@ -1,133 +1,160 @@
-let todo = [
-    {   
-        id:"1",
-        list:"do",
-        status:"false"
-    },
+let data = [
+    {
+        "id": 1,
+        "list":"Todo Listy",
+        "status": false
+    }
 ]
 
-
-
-
-
-let tbody = document.querySelector("#tbody");
-
+let tbody =document.getElementById("tbody")
 
 document.getElementById("form").addEventListener("submit",function(e){
+  e.preventDefault();
+   let id = document.getElementById("id").value
+  let todo_list = document.getElementById("todo_list").value
+    if(id){
 
-    e.preventDefault();
+     let updateData =   data.map((ele) => {
+
+            if(ele.id == id){
+                ele.list = document.getElementById("todo_list").value
+
+                document.getElementById("submit").innerHTML = "Enter Task"
+                document.getElementById("submit").style.background = "#380E60"
+              
+
+            }
+            return ele
+        })
+        showData(updateData)
+
+    }
   
+    else{
+         
 
-    let id = document.querySelector("#id").value;
+    let num = Math.random()
+    let obj = {
+        "id": Math.round(num*1000),
+        "list": todo_list,
+        "status": false
+    }
 
-if(id){
-    let updatedData = todo.map((ele) => {
+    data.push(obj)
+    showData(data)
+    }
 
-        if(ele.id == id){
-          ele.list = document.querySelector("#todo_list").value
-        }
 
-        return ele
-    })
-    console.log(updatedData)
-    showData(updatedData)
-}
-
-else{
-
-        let num = Math.random();
-         let obj = {
-        id:Math.round(num*1000),
-        list:document.querySelector("#todo_list").value,
-        status:false
-        }
-    todo.push(obj)
-    showData(todo)
-
-    
-}console.log(todo)
-
-    document.getElementById("todo_list").value =""
+document.getElementById("id").value =""
+    document.getElementById("todo_list").value =""                
 })
 
-function update(id){
+function lname(){
+    let subproduct = data.sort((a,b) => a.id - b.id)
+    showData(subproduct)
+}
+function complete(){
+    let subproduct = data.filter((ele) => ele.status == true)
 
-    todo.filter((ele) => {
+    showData(subproduct)
+}
+function ncomplete(){
+    let subproduct = data.filter((ele) => ele.status != true)
 
-        if(ele.id== id){
-            document.getElementById("todo_list").value = ele.list
-            document.getElementById("id").value = ele.id
-        }
-    })
-
-    document.getElementById("submit").innerHTML="update";
-    document.getElementById("submit").className="btn btn-warning"
-
+    showData(subproduct)
+}
+function hname(){
+    let subproduct = data.sort((b,a) => b.id - a.id)
+    showData(subproduct)
 }
 
 function dele(id){
 
-    let deleData = todo.filter((ele) => ele.id != id)
-    todo = deleData
+    let deleData = data.filter((ele) => ele.id !=id)
+
+    data = deleData
     showData(deleData)
 }
 
-function showData(deleData){
+function check(id){
+    let statusData = data.map((ele) => {
+        if(ele.id === id){
+            ele.status = !ele.status;
+        }
+         return ele;
+    })
+    
+     showData(statusData);
+    
+    console.log(statusData)
+    }
+function update(id){
+    let updatedData = data.filter((ele) =>{
+        if( ele.id == id){
+            
+        document.getElementById("todo_list").value = ele.list
+        document.getElementById("id").value = ele.id
+        }
+    } )
 
-    tbody.innerHTML =""
-   
+    document.getElementById("submit").innerHTML = "update"
+    document.getElementById("submit").style.background = "#FFC107"
 
-    deleData.map((ele) => {
+    // document.getElementById("todo_list").value =""
+    // document.getElementById("id").value = ""
+}
+
+function showData(data){
+    tbody.innerHTML=""
+
+    data.map((ele) => {
 
         tbody.innerHTML += `
-        
-                 <tr>
-                            <td>
-                                ${ele.list}
-                            </td>
-                            <td>
-                             <div class="form-check  d-flex  justify-content-center ">
-      <input class="form-check-input p-2" type="checkbox" value="" id="flexCheckChecked" >
+                       <tr class=" fs-4     ${ele.status ? 'table-success' : 'table-danger' }" id="box">
+                            <td class="" >
+   <input id="stat" onchange="check(${ele.id})" value="true" type="checkbox" ${ele.status ? "checked" : ""} class="status1" />
     
-    </div>   
-                            </td>
-                            <td>
-                                <button class="btn btn-warning" onclick=update(${ele.id})>Update</button>
-                            </td>
-                            <td>
-                                 <button class="btn btn-danger" onclick=dele(${ele.id})>Delete</button>
-                            </td>
+    
+    
+    </td>
+                            <td>${ele.list}</td>
+                            <td><button onclick="update(${ele.id})" class="btn btn-yellow fs-2" ><i class="ri-ball-pen-line"></i></button></td>
+                            <td><button onclick="dele(${ele.id})" class="btn btn-red fs-2" ><i class="ri-close-large-line"></i></i></button></td>
                         </tr>
         `
     })
-
-
-
 }
 
 
-todo.map((ele) => {
+
+
+data.map((ele) => {
 
     tbody.innerHTML += `
-    
-             <tr>
-                        <td>
-                            ${ele.list}
-                        </td>
-                        <td>
-                         <div class="form-check  d-flex  justify-content-center ">
-  <input class="form-check-input p-2" type="checkbox" value="" id="flexCheckChecked" >
 
-</div>   
-                        </td>
-                        <td>
-                            <button class="btn btn-warning" onclick=update(${ele.id})>Update</button>
-                        </td>
-                        <td>
-                            <button class="btn btn-danger" onclick=dele(${ele.id})>Delete</button>
-                        </td>
+          <tr class="  fs-4     ${ele.status ? 'table-success ' : 'table-danger' }" id="box">
+                     
+<td class="" >
+<input id="stat" onchange="check(${ele.id})" value="true" type="checkbox" ${ele.status ? "checked" : ""} class="status1 " />
+
+</td>
+                        <td>${ele.list}</td>
+                        <td><button onclick="update(${ele.id})" class="btn btn-yellow fs-2" ><i class="ri-ball-pen-line"></i></button></td>
+                        <td><button onclick="dele(${ele.id})" class="btn btn-red fs-2" ><i class="ri-close-large-line"></i></i></button></td>
                     </tr>
     `
 })
 
 
+setInterval( function(){
+  
+    
+
+let time = new Date()
+document.getElementById("time").innerHTML = `
+ ${time.getHours()}
+${time.getMinutes()}
+${time.getSeconds()}
+`
+},1000
+)
